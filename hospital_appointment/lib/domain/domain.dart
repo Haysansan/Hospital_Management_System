31ea49d6f8ad05605f53f40faeb6cdd767e4c66f
Haysansan <haysan.eang@student.cadt.edu.kh>
2025-10-30 01:26:16 +0700

    Person update

---
+++ hospital_appointment/lib/domain/domain.dart
@@ -0,0 +1,15 @@
+class Person {
+  final int id;
+  final String name;
+  final int age;
+  final String gender;
+  final String contactNumber;
+
+  Person({
+    required this.id,
+    required this.name,
+    required this.age,
+    required this.gender,
+    required this.contactNumber,
+  });
+}
